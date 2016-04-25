9b4d5cc7851d9386fd35539677732537f76522fc
wladek.airo@gmail.com <wladek.airo@gmail.com>
2016-04-25 19:36:24 +0300

    property

--- src/main/java/com/wladek/realestate/web/admin/PropertyController.java
+++ src/main/java/com/wladek/realestate/web/admin/PropertyController.java
@@ -19,7 +19,7 @@ import java.util.List;
 
 @Controller
 @RequestMapping(value = "/admin/pension")
-public class AssetController {
+public class PropertyController {
 
     @Autowired
     EmployeeValidator employeeValidator;
